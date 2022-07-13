bd13adc8fe47f8ee791f9ca30f9a256a56c5312b
merlinsdog <51785801+merlinsdog@users.noreply.github.com>
2022-07-13 13:58:32 +0100

    Add files via upload

---
+++ install-stuff-2022.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+pacman -S --noconfirm git screen nmap vim exfat-utils ntfs-3g intel-ucode rdesktop unrar p7zip rsync openssh etckeeper sshfs cups libmtp mtpfs android-udev android-tools python openvpn w3m newsboat mutt youtube-dl ffmpeg htop dmidecode vpnc net-tools atomicparsley unzip ddrescue testdisk gvfs-smb gvfs-nfs mlocate wget dosfstools htop tk minicom clamav
+
+pacman -S --noconfirm gparted vlc firefox audacious audacity thunderbird ttf-liberation ttf-droid ttf-dejavu ttf-ubuntu-font-family remmina xarchiver terminator libreoffice-fresh-en-GB x2goclient gtk3-print-backends cups-pdf gtk3-print-backends gvfs-mtp owncloud-client chromium epiphany gimp meld evince alsa-utils pulseaudio pavucontrol cdrtools libisoburn cdrdao dvd+rw-tools 
+
+pacman -S --noconfirm xed htop links elinks lynx vivaldi filezilla 
