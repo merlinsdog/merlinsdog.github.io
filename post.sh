3e93fafe03827c04e80f2ffbeadd3038d367031e
rowstu <51785801+rowstu@users.noreply.github.com>
2023-11-14 15:31:40 +0000

    Create post.sh

---
+++ post.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+pacman -S nmap openssh sshfs gparted rdesktop python wget screen ncdu gnu-netcat
+
+pacman -S exfat-utils ntfs-3g intel-ucode p7zip rsync unzip ddrescue testdisk gvfs-smb gvfs-nfs mlocate dosfstools tk minicom clamav
+
+pacman -S libmtp mtpfs android-udev android-tools youtube-dl ffmpeg dmidecode atomicparsley 
+
+pacman -S  vlc audacious audacity remmina freerdp xarchiver gvfs-mtp gimp meld alsa-utils pulseaudio cdrtools libisoburn cdrdao dvd+rw-tools 
+
+pacman -S  firefox chromium epiphany links elinks lynx w3m
+
+pacman -S  terminator xterm zsh zsh-completions bash-completion gnome-terminal vim
+
+pacman -S  libreoffice-fresh-en-gb evince
+
+pacman -S  xed htop filezilla thunderbird mutt newsboat
+
+pacman -S  xorg-server xorg-apps xorg-xinit xorg-xauth archlinux-wallpaper
+
+pacman -S xfce4 xfce4-goodies alacarte libcanberra xfce4-pulseaudio-plugin xfce4-terminal pavucontrol
+
+pacman -S  nemo-share nemo-terminal nemo-python nemo-fileroller
+
+pacman -S  virtualbox virtualbox-guest-iso virtualbox-host-dkms
+
+pacman -S  ttf-bitstream-vera ttf-liberation ttf-droid ttf-dejavu
+
+pacman -S  networkmanager-vpnc networkmanager-pptp net-tools vpnc
+
+pacman -S  gnome-calculator gnome-keyring gnome-backgrounds gnome-themes-extra flameshot
+
+pacman -S  cups cups-pdf hplip
+
+pacman -S  chrony
+
+pacman -S  dconf-editor
+
+pacman -S  usbutils gnome-disk-utility
+
+pacman -S  openvpn networkmanager-openvpn
+
+pacman -S man-db man-pages
+
+pacman -S keepassxc nextcloud-client android-tools yt-dlp tigervnc jq neofetch
+
+echo "optional install xf86-video-intel vscodium-bin proton-bridge signal-desktop-beta-bin github-desktop-bin clipit brave-bin google-chrome virtualbox-ext-oracle spotify-adblock zotify fslint-gui"
+
+
+echo "add exec cinnamon-session to .xinitrc"
